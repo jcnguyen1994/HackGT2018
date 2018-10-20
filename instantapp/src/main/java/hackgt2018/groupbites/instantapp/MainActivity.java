1bea60310481ae8931a65cdb37240116f8b93bb8
J <jcnguyen1994@gmail.com>
2018-10-20 07:05:53 -0400

    Button & Layout
    Functionalities:
    -Added button from MainActivity to go to HostActivity
    -Removed Android status bar, action bar, and navigation bar

--- instantapp/src/main/java/hackgt2018/groupbites/instantapp/MainActivity.java
+++ instantapp/src/main/java/hackgt2018/groupbites/instantapp/MainActivity.java
@@ -1,17 +1,25 @@
 package hackgt2018.groupbites.instantapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button hostButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        hostButton = (Button) findViewById(R.id.host_button);
+        hostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHostActivity();
+            }
+        });
         //Two methods below are to hide preset navigation bar/action bar/status bar
         getSupportActionBar().hide();
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
@@ -33,5 +41,10 @@ public class MainActivity extends AppCompatActivity {
                             | View.SYSTEM_UI_FLAG_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
+    }
+
+    public void openHostActivity(){
+        Intent intent = new Intent(this, HostActivity.class);
+        startActivity(intent);
     }
 }
